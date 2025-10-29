69f220608a289dfab1038cd51e50c7f320888df6
Prathmesh2806 <prathamesh.pawar.7707@gmail.com>
2025-10-29 11:36:34 +0530

    Update Task 25.sh

--- Version 3/Task 25.sh
+++ Version 3/Task 25.sh
@@ -11,6 +11,18 @@
 ############################################################################
 
 #!/bin/bash
+function USER_CREATED_REPORT() {
+    echo -e "Hello All, 
+   This is system generated email, do not reply to this email.
+   
+   The report of newly created user within last 24 hours on this system $(ifconfig | grep inet | head -n 1 | awk '{print $2}')
+   
+   Server Name : - $(hostname)
+   Server IP :- $(ifconfig | grep inet | head -n 1 | awk '{print $2}')
+   
+   Thank & Regards,
+   Team CloudEthix"
+}
 DIR="$1"
 DAYS="$2"
 if [ $# -lt 2 ]; then
@@ -26,6 +38,7 @@ USERREPORT=$(find "$DIR" -maxdepth 1 -mindepth 1 -type d -mtime "-$DAYS")
 
 if [ -n "$USERREPORT" ]; then
     echo -e "These are the new users created in the last $DAYS days under '$DIR':\n$USERREPORT"
+    USER_CREATED_REPORT
 else
     echo "There are no new users created in the last $DAYS days under '$DIR'."
 fi
